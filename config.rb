d087adc7f5ad9ff32ddcf9feddc9e48a7048d68f
Arjan Eising <arjan@arjaneising.nl>
2012-12-14 14:11:05 +0100

    Compressed assets

--- config.rb
+++ config.rb
@@ -8,14 +8,12 @@ images_dir = "images"
 javascripts_dir = "scripts"
 fonts_dir = "fonts"
 
-output_style = :nested
-environment = :development
+output_style = :compressed
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
 color_output = false
 
 
